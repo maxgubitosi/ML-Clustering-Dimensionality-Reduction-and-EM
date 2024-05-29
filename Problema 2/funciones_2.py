f02d9bf323ebcaea8011399bda2dee47dab66052
maxgubitosi <mgubitosi@udesa.edu.ar>
2024-05-29 10:23:00 -0300

    update_2

--- Problema 2/funciones_2.py
+++ Problema 2/funciones_2.py
@@ -4,6 +4,12 @@ import pandas as pd
 from tqdm import tqdm
 from scipy.stats import multivariate_normal
 
+# imports for VAE in PyTorch
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+# from torchvision import transforms
+from torch import nn
+from torch.nn import functional as F
 
 def plot_sample(data, i=None):
     if i is None:
@@ -72,3 +78,57 @@ class PCA:
 
     def compute_mse(self, X_original, X_reconstructed):
         return np.mean((X_original - X_reconstructed) ** 2)
+    
+
+
+def plot_reconstructions(df, X_re, n_samples=10):
+    fig, axes = plt.subplots(nrows=2, ncols=n_samples, figsize=(20, 4))  # 10 columns
+    for i in range(10):
+        sample = df.iloc[i].drop('label').values.reshape(28, 28)
+        reconstructed = X_re.iloc[i].values.reshape(28, 28)
+        # original image
+        ax = axes[0, i]
+        ax.imshow(sample, cmap='gray')
+        ax.set_title('Original')
+        ax.axis('off')
+        # reconstruced image
+        ax = axes[1, i]
+        ax.imshow(reconstructed, cmap='gray')
+        ax.set_title('Reconstructed')
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 20)  # mean
+        self.fc22 = nn.Linear(400, 20)  # log-variance
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE + KLD
